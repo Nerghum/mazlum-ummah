eace46c221977e5b329038e7030794e410c5ce7a
Nerghum <nerghum@gmail.com>
2026-07-22 00:07:04 +0600

    feat: grant user management access to Admin role

--- admin/src/utils/roles.js
+++ admin/src/utils/roles.js
@@ -1,6 +1,6 @@
 export const permissions = {
   'Super Admin': ['*'],
-  Admin: ['dashboard:read', 'news:*', 'blog:*', 'mediaAchievement:*', 'socialPost:*', 'notice:*', 'advertisement:*', 'menu:*', 'faq:*', 'category:*', 'country:*', 'media:*', 'homepage:*', 'analytics:*', 'settings:*'],
+  Admin: ['dashboard:read', 'news:*', 'blog:*', 'mediaAchievement:*', 'socialPost:*', 'notice:*', 'advertisement:*', 'menu:*', 'faq:*', 'category:*', 'country:*', 'media:*', 'homepage:*', 'analytics:*', 'settings:*', 'user:*'],
   Editor: ['dashboard:read', 'news:*', 'blog:*', 'mediaAchievement:*', 'socialPost:*', 'notice:*', 'media:*', 'category:read', 'country:read'],
   Journalist: ['dashboard:read', 'news:create', 'news:read', 'news:update-own', 'news:delete-own', 'blog:create', 'blog:read', 'blog:update-own', 'blog:delete-own', 'media:create', 'media:read', 'category:read', 'country:read'],
   Moderator: ['dashboard:read', 'news:read', 'blog:read', 'mediaAchievement:read', 'socialPost:read', 'notice:read', 'comment:*', 'analytics:read']
